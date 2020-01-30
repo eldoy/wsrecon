7f140f66c5f06a7cba1c68e83835553f4fc1a74f
Vidar Eldøy <vidar@eldoy.com>
2020-01-30 04:27:30 +0100

    Update version, clean up build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,18 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const uglify = require('uglify-es')
+
+console.log('Building...')
+
 const lib = require('../index.js')
 const fn = 'socket'
 const code = `window.${fn} = ${lib.toString()}\n`
-
 if (!fs.existsSync('./dist')) {
   fs.mkdirSync('./dist')
 }
-
 fs.writeFileSync(`./dist/${fn}.js`, code)
+
+console.log('Minifying...')
+
+const result = uglify.minify(code)
+fs.writeFileSync(`./dist/${fn}-min.js`, result.code)
